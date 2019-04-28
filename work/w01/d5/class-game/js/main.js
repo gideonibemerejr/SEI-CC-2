a2f323e5ab1e3b50a5108715c2919649e352efe4
Gideon Ibemere Jr <goij@Gideons-MacBook-Pro.local>
2019-04-27 23:50:33 -0500

    add console logging for win logic

--- work/w01/d5/class-game/js/main.js
+++ work/w01/d5/class-game/js/main.js
@@ -5,7 +5,6 @@ const colors = {
     '0': 'white'
 };
 
-
 /*----- app's state (variables) -----*/
 
 let board, winner, turn;
@@ -19,27 +18,30 @@ const feedback = document.getElementById('feedback');
 document.getElementById('col-markers').addEventListener('click',handleClick);
 
 
-
-
-
-
-
 /*----- functions -----*/
 init();
-
 
 //when the user clicks 
 function handleClick(event) {
     const marker = event.target;
     const colIdx = parseInt(marker.id.replace('col', ''));
     if (isNaN(colIdx)) return;
-    console.log(colIdx);
     const rowIdx = board[colIdx].indexOf(0);
     if (rowIdx === -1) return;
     board[colIdx][rowIdx] = turn;
-    //TODO:  winner = getWinner();
+    winner = getWinner();
+    console.log(winner);
     turn *= -1;
     render();
+}
+
+function getWinner() {
+    let winner = null;
+    for(let colIdx = 0; colIdx < board.length; colIdx++){
+        winner = checkCol(colIdx);
+        if (winner) break;
+    }
+    return winner;
 }
 
 // render the game to the DOM with this function
@@ -48,7 +50,7 @@ function render() {
     board.forEach(function(colArr, colIdx) {
         // update the markers 
         const marker = document.getElementById(`col${colIdx}`);
-        marker.style.borderTopColor = colArr.includes(0) ? "gray" : "white"
+        marker.style.borderTopColor = colArr.includes(0) ? "gray" : "white";
         colArr.forEach(function(cell, rowIdx){
             // access the correct div in the section
             const div = document.getElementById(`c${colIdx}r${rowIdx}`);
@@ -57,8 +59,12 @@ function render() {
     });
     // display feedback
     if(winner) {
+
         if(winner === 'T'){
             feedback.textContent = "LMAO that's wild, y'all tied...";
+        } 
+        if (winner === 1 || winner === -1){
+            feedback.textContent = `${colors[turn].toUpperCase()} Wins!`
         }
     } else {
         feedback.textContent = `${colors[turn].toUpperCase()}'s turn!`
@@ -66,16 +72,47 @@ function render() {
 };
 
 
+
+function checkCol(colIdx) {
+    // iterate through each column (board[colIdx]) to see if the absolute value of the first 4 elements is equal to four
+      //if they're not return null if the sum is equal to for run colorCheck
+    /*---- 
+    let winner = null;
+    sum = 0;
+    board[colIdx].forEach(function(element, rowIdx){
+        while (rowIdx <= 7)
+            sum += element);
+        }
+        if (sum < 4) {
+            return winner;
+        } else if(sum ? 4 : -4 ){
+            return winner = sum/4;
+        } else {
+            return winner = 'T';
+        }
+        change to absolute value
+
+    });
+    ---*/
+    let winner = null;
+    sum = 0;
+    board[colIdx].forEach(function(element){
+        console.log(element);
+       
+    });
+          
+};
+
 // initialize the game with this function and render the board to the DOM
 function init() {
     board = [
+        [0, 0, 0, 0, 0, 0],//column 0
         [0, 0, 0, 0, 0, 0],//column 1
         [0, 0, 0, 0, 0, 0],//column 2
         [0, 0, 0, 0, 0, 0],//column 3
         [0, 0, 0, 0, 0, 0],//column 4
         [0, 0, 0, 0, 0, 0],//column 5
-        [0, 0, 0, 0, 0, 0],//column 6
-        [0, 0, 0, 0, 0, 0]//column 7
+        [0, 0, 0, 0, 0, 0]//column 6
     ];
     winner = null;
     turn = 1;
