aa48b143ccbbd6102b8c3d0f59729bddc7faed3f
Gideon Ibemere Jr <goij@Gideons-MacBook-Pro.local>
2019-05-02 09:33:32 -0500

    Edit to Unit Project

---
+++ work/hw-w02/unit-project/js/main.js
@@ -0,0 +1,14 @@
+class Artist = {
+    constructor(name, song, spotify, youtube, apple, emoji) {
+        this.name = name;
+        this.song = song;
+        this.emoji = emoji;
+        this.spotify = spotify;
+        this.apple = apple;
+        this.youtube = youtube;
+    }
+    mountSong(){
+
+    }
+}
+const Skepta = new Artist('Skepta', "")
