01764b99ee1d897a325ddee8a39eb1e9471d4824
Gideon Ibemere Jr <goij@goij-pro.local>
2019-05-14 13:17:05 -0500

    Initialize Lab Part 1

--- work/h2-w04/d1/server.js
+++ work/h2-w04/d1/server.js
@@ -1,37 +1,40 @@
 //Load express
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 // artist database
-const artistDb = require("./data/artist-db");
+const artistDb = require('./data/artist-db');
 
 //create the express app
 const app = express();
 
 // Configure the app (app.set)
-app.set("view engine", "twig");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'twig');
+app.set('views', path.join(__dirname, 'views'));
 
 // Mount middleware (app.use)
 
 // Mount Routes
-app.get("/", function(req, res) {
-  res.redirect("/home");
+app.get('/', function(req, res) {
+  res.redirect('/home');
 });
 
-app.get("/home", function(req, res) {
-  res.render("home");
+app.get('/home', function(req, res) {
+  res.render('home');
 });
 
-app.get("/artists", function(req, res) {
-  res.render("artists/index", {
+app.get('/artists', function(req, res) {
+  res.render('artists/index', {
     artists: artistDb.getAll()
   });
 });
 
-app.get("/artists/:id", function(req, res) {
+app.get('/artists/:id', function(req, res) {
   console.log(`The value for the :id rout parameter is: ${req.params.id}`);
-  res.render("artists/show", { artist: artistDb.getOne(req.params.id) });
+  res.render('artists/show', {
+    artist: artistDb.getOne(req.params.id),
+    todoNum: parseInt(req.params.id) + 1
+  });
 });
 
 //Tell the app to list on port 3000
