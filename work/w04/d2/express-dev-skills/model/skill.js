f1ea118219b7f778427015d1d2ba6a194af7fc6d
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-05-16 11:40:15 -0500

    Lab complete

--- work/w04/d2/express-dev-skills/model/skill.js
+++ work/w04/d2/express-dev-skills/model/skill.js
@@ -45,8 +45,23 @@ const skills = [
 
 module.exports = {
   getAll,
-  getOne
+  getOne,
+  create,
+  deleteOne,
+  update
 };
+
+function update(id, skill) {
+  skills.splice(id, 1, skill);
+}
+
+function deleteOne(id) {
+  skills.splice(id, 1);
+}
+
+function create(skill) {
+  skills.push(skill);
+}
 
 function getOne(id) {
   return skills[id];
--- work/w04/d2/express-dev-skills/routes/skills.js
+++ work/w04/d2/express-dev-skills/routes/skills.js
@@ -4,7 +4,13 @@ const skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
 router.get('/', skillsCtrl.index);
-
+router.get("/new", skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
+router.post("/", skillsCtrl.create);
+router.delete('/:id', skillsCtrl.delete);
+router.get('/:id/edit', skillsCtrl.edit);
+router.put('/:id', skillsCtrl.update);
+
+
 
 module.exports = router;
