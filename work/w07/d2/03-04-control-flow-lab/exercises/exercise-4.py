cb7b98fbb3492504ceb61bc09de3d987f32f2b2f
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-04 14:11:22 -0500

    Exercise 4 complete

--- work/w07/d2/03-04-control-flow-lab/exercises/exercise-4.py
+++ work/w07/d2/03-04-control-flow-lab/exercises/exercise-4.py
@@ -3,12 +3,25 @@
 # Write the code that:
 # 1. Prompts the user to enter the three lengths of a triangle (one at a time):
 #      Enter the lengths of three side of a triangle:
-#      a: 
+#      a:
 #      b:
 #      c:
+print('Enter the lengths of three side of a triangle: ')
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
 # 2. Write the code that determines if the triangle is:
-#      equalateral - all three sides are equal in length
+#      equilateral - all three sides are equal in length
 #      scalene - all three sides are unequal in length
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+if a == b and b == c:
+    print(
+        f'A triangle with sides of {a}, {b} & {c} is an equilateral triangle')
+elif a != b and a != c and b != c:
+    print(
+        f'A triangle with sides of {a}, {b} & {c} is a scalene triangle')
+else:
+    print(
+        f'A triangle with sides of {a}, {b} & {c} is an isosceles triangle')
