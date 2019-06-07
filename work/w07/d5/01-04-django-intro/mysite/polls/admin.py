ea636553ac1b4b7b0dc2d0dd8cd7c943da017fc3
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-07 17:02:19 -0500

    a

--- work/w07/d5/01-04-django-intro/mysite/polls/admin.py
+++ work/w07/d5/01-04-django-intro/mysite/polls/admin.py
@@ -3,5 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register([Question, Choice])
--- work/w07/d5/01-04-django-intro/mysite/polls/views.py
+++ work/w07/d5/01-04-django-intro/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 # Create your views here.
@@ -6,11 +6,12 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return HttpResponse("You're looking at question %s." % question_id)
 
 
