2f5b6281e51ffbb782bc287eac702729d8920928
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-10 12:35:02 -0500

    Class lesson complete

--- work/w08/d1/01-django-urls-views-templates/catcollector/main_app/models.py
+++ work/w08/d1/01-django-urls-views-templates/catcollector/main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
--- work/w08/d1/01-django-urls-views-templates/catcollector/main_app/views.py
+++ work/w08/d1/01-django-urls-views-templates/catcollector/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat, cats
 
 
 def home(request):
@@ -7,5 +8,9 @@ def home(request):
 
 
 def about(request):
-    return render(request, 'about/about.html', {'title': 'About Cat Collector'})
+    return render(request, 'about.html', {'title': 'About Cat Collector'})
 # Create your views here.
+
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'title': 'Cats | Cat Collector', 'cats': cats})
