bb3cfec6cadd3cdaccc4fc85860375335079fcde
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-11 11:58:52 -0500

    Finish in-class lesson

--- work/w08/d1/03-intro-to-django-models/starter-code/catcollector/main_app/urls.py
+++ work/w08/d1/03-intro-to-django-models/starter-code/catcollector/main_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  path('cats/', views.cats_index, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='index'),
+    path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
 ]
--- work/w08/d1/03-intro-to-django-models/starter-code/catcollector/main_app/views.py
+++ work/w08/d1/03-intro-to-django-models/starter-code/catcollector/main_app/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
-
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+from .models import Cat
 
 # View functions
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
+
 def cats_index(request):
-  return render(request, 'cats/index.html', { 'cats': cats })
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
