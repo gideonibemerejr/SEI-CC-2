c47446f30b1ddd6fe709388de4ab20aaf4d3c293
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-11 16:42:49 -0500

    In-class lesson complete

--- work/w08/d2/03-04-django-one-to-many-models/starter-code/catcollector/main_app/views.py
+++ work/w08/d2/03-04-django-one-to-many-models/starter-code/catcollector/main_app/views.py
@@ -1,29 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
+
 
 class CatCreate(CreateView):
-  model = Cat
-  fields = '__all__'
+    model = Cat
+    fields = '__all__'
+
 
 class CatUpdate(UpdateView):
-  model = Cat
-  fields = ['breed', 'description', 'age']
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
 
 class CatDelete(DeleteView):
-  model = Cat
-  success_url = '/cats/'
+    model = Cat
+    success_url = '/cats/'
+
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def cats_index(request):
-  cats = Cat.objects.all()
-  return render(request, 'cats/index.html', { 'cats': cats })
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
 
 def cats_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', { 'cat': cat })
+    cat = Cat.objects.get(id=cat_id)
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form})
+
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
