8168749e9ad27c95af6db0151f3a57b31a447b0a
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-12 11:32:21 -0500

    Repl associations

--- work/w08/d3/01-02-django-many-to-many-models/starter-code/catcollector/main_app/models.py
+++ work/w08/d3/01-02-django-many-to-many-models/starter-code/catcollector/main_app/models.py
@@ -23,6 +23,7 @@ class Cat(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
