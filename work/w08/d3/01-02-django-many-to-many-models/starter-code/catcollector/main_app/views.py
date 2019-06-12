e6005fec1e671fd29c219a895238547dda1daeca
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-12 17:33:05 -0500

    AWS Upload Works

--- work/w08/d3/01-02-django-many-to-many-models/starter-code/catcollector/main_app/views.py
+++ work/w08/d3/01-02-django-many-to-many-models/starter-code/catcollector/main_app/views.py
@@ -4,63 +4,84 @@ from django.views.generic import ListView, DetailView
 from .models import Cat, Toy
 from .forms import FeedingForm
 
+
 class CatCreate(CreateView):
-  model = Cat
-  fields = '__all__'
+    model = Cat
+    fields = '__all__'
+
 
 class CatUpdate(UpdateView):
-  model = Cat
-  fields = ['breed', 'description', 'age']
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
 
 class CatDelete(DeleteView):
-  model = Cat
-  success_url = '/cats/'
+    model = Cat
+    success_url = '/cats/'
+
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def cats_index(request):
-  cats = Cat.objects.all()
-  return render(request, 'cats/index.html', { 'cats': cats })
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
 
 def cats_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  # instantiate FeedingForm to be rendered in the template
-  feeding_form = FeedingForm()
-  return render(request, 'cats/detail.html', {
-    # pass the cat and feeding_form as context
-    'cat': cat, 'feeding_form': feeding_form
-  })
+    cat = Cat.objects.get(id=cat_id)
+    # instantiate FeedingForm to be rendered in the template
+    toys_cat_doesnt_have = Toy.objects.exclude(
+        id__in = cat.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # pass the cat and feeding_form as context
+        'cat': cat, 'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have
+    })
+
 
 def add_feeding(request, cat_id):
-	# create the ModelForm using the data in request.POST
-  form = FeedingForm(request.POST)
-  # validate the form
-  if form.is_valid():
-    # don't save the form to the db until it
-    # has the cat_id assigned
-    new_feeding = form.save(commit=False)
-    new_feeding.cat_id = cat_id
-    new_feeding.save()
-  return redirect('detail', cat_id=cat_id)
+        # create the ModelForm using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
+
+
+def assoc_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('detail', cat_id=cat_id)
+
 
 class ToyList(ListView):
-  model = Toy
+    model = Toy
+
 
 class ToyDetail(DetailView):
-  model = Toy
+    model = Toy
+
 
 class ToyCreate(CreateView):
-  model = Toy
-  fields = '__all__'
+    model = Toy
+    fields = '__all__'
+
 
 class ToyUpdate(UpdateView):
-  model = Toy
-  fields = ['name', 'color']
+    model = Toy
+    fields = ['name', 'color']
+
 
 class ToyDelete(DeleteView):
-  model = Toy
-  success_url = '/toys/'
+    model = Toy
+    success_url = '/toys/'
