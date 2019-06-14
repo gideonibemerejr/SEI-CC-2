145f07046b85a0de236b108cbd1ff693b7f3db72
Gideon Ibemere Jr <goij@goij-pro.local>
2019-06-14 09:24:30 -0500

    Complete in class lab

--- work/w08/d4/02-03-django-authentication/starter-code/catcollector/main_app/forms.py
+++ work/w08/d4/02-03-django-authentication/starter-code/catcollector/main_app/forms.py
@@ -1,7 +1,22 @@
-from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import Feeding
+from django.contrib.auth.models import User
+
+ModelForm = forms.ModelForm
+
 
 class FeedingForm(ModelForm):
-  class Meta:
-    model = Feeding
-    fields = ['date', 'meal']
+    class Meta:
+        model = Feeding
+        fields = ['date', 'meal']
+
+
+class FullUserForm(UserCreationForm):
+    first_name = forms.CharField(max_length=40)
+    first_name = forms.CharField(max_length=40)
+    email = forms.EmailField(max_length=75)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
--- work/w08/d4/02-03-django-authentication/starter-code/catcollector/main_app/views.py
+++ work/w08/d4/02-03-django-authentication/starter-code/catcollector/main_app/views.py
@@ -1,102 +1,162 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 import uuid
 import boto3
 from .models import Cat, Toy, Photo
-from .forms import FeedingForm
+from .forms import FeedingForm, FullUserForm
+
 
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'catcollector'
 
-class CatCreate(CreateView):
-  model = Cat
-  fields = ['name', 'breed', 'description', 'age']
 
-class CatUpdate(UpdateView):
-  model = Cat
-  fields = ['breed', 'description', 'age']
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
 
-class CatDelete(DeleteView):
-  model = Cat
-  success_url = '/cats/'
+    # This inherited method is called when a
+    # valid cat form is being submitted
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
+
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
+
+@login_required
 def cats_index(request):
-  cats = Cat.objects.all()
-  return render(request, 'cats/index.html', { 'cats': cats })
+    cats = Cat.objects.filter(user=request.user)
+    return render(request, 'cats/index.html', {'cats': cats})
 
+
+@login_required
 def cats_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  # Get the toys the cat doesn't have
-  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
-  # Instantiate FeedingForm to be rendered in the template
-  feeding_form = FeedingForm()
-  return render(request, 'cats/detail.html', {
-    # Pass the cat and feeding_form as context
-    'cat': cat, 'feeding_form': feeding_form,
-    # Add the toys to be displayed
-    'toys': toys_cat_doesnt_have
-  })
+    cat = Cat.objects.get(id=cat_id)
+    # Get the toys the cat doesn't have
+    toys_cat_doesnt_have = Toy.objects.exclude(
+        id__in=cat.toys.all().values_list('id'))
+    # Instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # Pass the cat and feeding_form as context
+        'cat': cat, 'feeding_form': feeding_form,
+        # Add the toys to be displayed
+        'toys': toys_cat_doesnt_have
+    })
 
+
+@login_required
 def add_feeding(request, cat_id):
-	# create the ModelForm using the data in request.POST
-  form = FeedingForm(request.POST)
-  # validate the form
-  if form.is_valid():
-    # don't save the form to the db until it
-    # has the cat_id assigned
-    new_feeding = form.save(commit=False)
-    new_feeding.cat_id = cat_id
-    new_feeding.save()
-  return redirect('detail', cat_id=cat_id)
+        # create the ModelForm using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
 
+
+@login_required
 def add_photo(request, cat_id):
-	# photo-file was the "name" attribute on the <input type="file">
-  photo_file = request.FILES.get('photo-file', None)
-  if photo_file:
-    s3 = boto3.client('s3')
-    # need a unique "key" for S3 / needs image file extension too
-    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-    # just in case something goes wrong
-    try:
-      s3.upload_fileobj(photo_file, BUCKET, key)
-      # build the full url string
-      url = f"{S3_BASE_URL}{BUCKET}/{key}"
-      # we can assign to cat_id or cat (if you have a cat object)
-      photo = Photo(url=url, cat_id=cat_id)
-      photo.save()
-    except:
-      print('An error occurred uploading file to S3')
-  return redirect('detail', cat_id=cat_id)
+        # photo-file was the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + \
+            photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url, cat_id=cat_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', cat_id=cat_id)
 
+
+@login_required
 def assoc_toy(request, cat_id, toy_id):
-  Cat.objects.get(id=cat_id).toys.add(toy_id)
-  return redirect('detail', cat_id=cat_id)
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('detail', cat_id=cat_id)
 
+
+@login_required
 def unassoc_toy(request, cat_id, toy_id):
-  Cat.objects.get(id=cat_id).toys.remove(toy_id)
-  return redirect('detail', cat_id=cat_id)
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('detail', cat_id=cat_id)
 
-class ToyList(ListView):
-  model = Toy
 
-class ToyDetail(DetailView):
-  model = Toy
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = FullUserForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = FullUserForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
-class ToyCreate(CreateView):
-  model = Toy
-  fields = '__all__'
 
-class ToyUpdate(UpdateView):
-  model = Toy
-  fields = ['name', 'color']
+class ToyList(LoginRequiredMixin, ListView):
+    model = Toy
 
-class ToyDelete(DeleteView):
-  model = Toy
-  success_url = '/toys/'
+
+class ToyDetail(LoginRequiredMixin, DetailView):
+    model = Toy
+
+
+class ToyCreate(LoginRequiredMixin, CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(LoginRequiredMixin, DeleteView):
+    model = Toy
+    success_url = '/toys/'
