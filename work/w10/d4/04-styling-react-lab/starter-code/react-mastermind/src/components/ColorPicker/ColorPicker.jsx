882ed6d3a4919bc491facd56afa099f7bfdca7b4
jim-clark <email@jim-clark.com>
2019-06-18 06:26:34 -0700

    Add styling-react-lab

---
+++ work/w10/d4/04-styling-react-lab/starter-code/react-mastermind/src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ColorPicker.module.css';
+
+const ColorPicker = (props) => (
+  <div>
+    {props.colors.map((color, idx) =>
+      <button
+        color={color}
+        key={color}
+        className={styles.button}
+        style={{
+          backgroundColor: props.selColorIdx === idx ? 'white' : color,
+          borderColor: color
+        }}
+      />
+    )}
+  </div>
+);
+
+export default ColorPicker;
