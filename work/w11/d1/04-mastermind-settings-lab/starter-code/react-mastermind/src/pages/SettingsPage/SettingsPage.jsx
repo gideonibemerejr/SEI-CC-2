971c72adb5d4535a30f8d2d8fbacb2ddd224c0be
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-01 16:43:53 -0500

    Add level selector buttons

--- work/w11/d1/04-mastermind-settings-lab/starter-code/react-mastermind/src/pages/SettingsPage/SettingsPage.jsx
+++ work/w11/d1/04-mastermind-settings-lab/starter-code/react-mastermind/src/pages/SettingsPage/SettingsPage.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const SettingsPage = (props) => {
+const SettingsPage = props => {
+  const keys = Object.keys(props.colorsList)
+
+  const levels = keys.map(level => (
+    <div key={level} className="settings-row">
+      <button className="btn btn-default">{level}</button>
+    </div>
+  ))
   return (
     <div>
-      <Link to='/'>HOME</Link>
+      <Link to="/">HOME</Link>
       <h1>Settings Page</h1>
+      <div>{levels}</div>
     </div>
-  );
-};
+  )
+}
 
-export default SettingsPage;
+export default SettingsPage
