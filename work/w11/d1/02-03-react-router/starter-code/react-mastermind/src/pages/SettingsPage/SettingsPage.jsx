58d9d6d65b96c38b0b5e8b49513e8412395c33c1
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-01 16:50:41 -0500

    Update from class lesson

---
+++ work/w11/d1/02-03-react-router/starter-code/react-mastermind/src/pages/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const SettingsPage = props => (
+  <>
+    <Link to="/">HOME</Link>
+    <h1>Settings Page</h1>
+  </>
+)
+export default SettingsPage
