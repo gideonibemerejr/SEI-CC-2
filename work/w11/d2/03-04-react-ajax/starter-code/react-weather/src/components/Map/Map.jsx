3b6a823af6c2221a42aca3bdbded3484610b311f
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-02 16:14:27 -0500

    Finish in-class lesson

--- work/w11/d2/03-04-react-ajax/starter-code/react-weather/src/components/Map/Map.jsx
+++ work/w11/d2/03-04-react-ajax/starter-code/react-weather/src/components/Map/Map.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import styles from './Map.module.css';
-import mapStyle from './map-style';
+import React, { Component } from 'react'
+import styles from './Map.module.css'
+import mapStyle from './map-style'
 
-function Map({lat, lng, zoom}) {
-  // Create a ref object that exposes native DOM elements
-  const mapDiv = React.createRef();
+export default class Map extends Component {
+  mapDiv = React.createRef()
 
-  if (lat && lng) {
-    const map = new window.google.maps.Map(
-      mapDiv.current, {
-        zoom: zoom || 12,
-        center: {lat, lng},
+  setMap() {
+    if (this.props.lat && this.props.lng) {
+      const location = { lat: this.props.lat, lng: this.props.lng }
+      const map = new window.google.maps.Map(this.mapDiv.current, {
+        zoom: this.props.zoom || 12,
+        center: location,
         disableDefaultUI: true,
         styles: mapStyle
-      }
-    );
-    new window.google.maps.Marker({position: {lat, lng}, map: map});
+      })
+      new window.google.maps.Marker({ position: location, map: map })
+    }
+  }
+  
+  componentDidMount() {
+    this.setMap()
+  }
+  componentDidUpdate() {
+    this.setMap()
   }
 
-  return (
-    <div ref={mapDiv} className={styles.Map}></div>
-  );
+  render() {
+    return <div ref={this.mapDiv} className={styles.Map} />
+  }
 }
-
-export default Map;
